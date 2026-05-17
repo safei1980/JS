32b1707c83ec117242ce58659721c332f1150c72
safei1980 <ahmadsafei.com@gmail.com>
2026-05-17 15:02:57 +0700

    Add files via upload

---
+++ adatanggal.js
@@ -0,0 +1,17 @@
+function adatanggal(text)
+{
+	var dayarray = new Array("Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu");
+	var montharray = new Array("Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember");
+
+	var kursusblog = new Date();
+
+	var day = kursusblog.getDay();
+	var month = kursusblog.getMonth();
+	var daym = kursusblog.getDate();
+	var year = kursusblog.getFullYear();
+
+	if (daym < 10)
+		daym = "0" + daym;
+	
+        document.write(dayarray[day] + ", " + daym+" "+ montharray[month]+" "+year);
+}
